3a36c9504cbc3d64e4d411c9e2100b058526cafd
Yann Mulonda <yannmjl@gmail.com>
2018-06-15 16:20:33 -0500

    added comments to explain the command line

--- streamPiCam.sh
+++ streamPiCam.sh
@@ -3,4 +3,13 @@
 # ------------------------------------------------------------------------------------- #
 
 # notes:
+# "raspivid" the command line used to capture the video with the Pi camera
+
+# "-o -" causes the output to be written to stdout
+# "-t 0" sets the timeout to disabled (capture indefinitely)
+
+# "-w" width, available range: 64 to 1920 
+# "-h" Height, available range: 64 to 1080
+
 #!/bin/bash
+raspivid -o - -t 0 -w 640 -h 360 -fps 25 -n|cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8090}' :demux=h264
