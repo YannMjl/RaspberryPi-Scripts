6062665922b2a0c9a76374fae6b1322ac899ed50
Yann Mulonda <yannmjl@gmail.com>
2018-06-19 00:16:27 -0500

    Update sendPhoto.sh

--- sendPhoto.sh
+++ sendPhoto.sh
@@ -1,1 +1,33 @@
+# -------------------------------------------------------------------------------------------------------------------------- #
+# This script below will email a time & date stamped photo                                                                   #
+# -------------------------------------------------------------------------------------------------------------------------- #
 
+#!/bin/bash
+
+# create an infinite loop inside wwhich operations will be done
+# to continuously check for door status and send notifications
+while true; 
+do 
+
+    trap 'echo "4" > /sys/class/gpio/unexport' 0
+
+    # get the state value of the pin on the raspberry
+    stat=`cat /sys/class/gpio/gpio4/value`
+
+    while [$stat = "1"]
+    do
+        # get date and time
+        date=`date +%d-%m-%Y`
+        time=`date '+%H:%M'`
+
+        # display door status
+        echo "Door opened at $time on $date"
+    done
+    
+    # add a waiting time reduce the the processor use by over 90%
+    sleep 0.09
+
+done
+
+exit 0
+# -------------------------------------------------------------------------------------------------------------------------- #
