22168267f833ffe0ba7f153803ed96def2700950
Yann Mulonda <yannmjl@gmail.com>
2018-11-19 23:40:41 -0600

    Update streamPiCam.sh

--- scripts/streamPiCam.sh
+++ scripts/streamPiCam.sh
@@ -6,13 +6,10 @@
 # "raspivid" the command line used to capture the video with the Pi camera
 # "-o -" causes the output to be written to stdout
 # "-t 0" sets the timeout to disabled (capture indefinitely)
-
 # "-w" width, available range: 64 to 1920
 # "-h" Height, available range: 64 to 1080
-
 # "-fps" Frames per second, 2 to 30 (may change in the future)
 # "-n" stops the video being previewed (remove if you want to see the video on the HDMI output)
-
 # "cvlc" is the console vlc player
 # "-vvv" specifies where to get the stream from
 # "-sout" specifies where to output it to
