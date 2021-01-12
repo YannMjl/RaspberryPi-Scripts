984fa211140b5bbf896acc122ca91519e2b3a172
Yann Mulonda <yannmjl@gmail.com>
2021-01-12 11:31:50 -0600

    Update streamPiCam.sh

--- scripts/streamPiCam.sh
+++ scripts/streamPiCam.sh
@@ -15,6 +15,7 @@
 # -------------------------------------------------------------------------------------------------------------------------- #
 
 #!/bin/bash
+
 # stream over HTTP
 # raspivid -o - -t 0 -w 640 -h 360 -fps 25 -n|cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8090}' :demux=h264
 
